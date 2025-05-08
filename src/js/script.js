702be189e87f87b38ef3272de6f270f7898db437
Christoffer <christofferalves10@gmail.com>
2025-05-08 09:45:29 -0300

    Criando o JavaScript do projeto

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+// Declarando o array de imagens
+
+let imagens =[
+    'src/assets/imagem1.jpg',
+    'src/assets/imagem2.jpg',
+    'src/assets/imagem3.jpg',
+];
+
+
+// Declarando as variáveis
+
+let index=0;
+let tempo=3000;
+
+
+// Criando a função SLIDESHOW
+
+function slideShow(){
+    document.getElementById("image").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout("slideShow()", tempo)
+}
+slideShow();
